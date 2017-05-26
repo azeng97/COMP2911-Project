8b89f86243714c493395bed8cffb19d6a79d4567
Avan Zeng <a.zeng@student.unsw.edu.au>
2017-05-26 15:26:28 +1000

    undos working

--- src/Player.java
+++ src/Player.java
@@ -13,13 +13,26 @@ public class Player extends Entity{
 		Square newSquare = game.squareAt(newPos);
 		if(newSquare.pushContents(direction))
 		{
-			return newSquare.addEntity(this);
+			m.setPushed(true);
+			return newSquare.addEntity(this);			
 		}
 		else if (newSquare.isEmpty())
 		{
 			return newSquare.addEntity(this);
 		}
 		return false;
+	}
+	
+	public void undoMove(Move m)
+	{
+		int direction1 = m.getDirection();
+		Position boxPos = (Position) square.getPosition().adjacentPos(direction1);
+		int direction = Position.reverseDirection(m.getDirection());
+		Position newPos = (Position) square.getPosition().adjacentPos(direction);
+		Square boxSquare = game.squareAt(boxPos);
+		Square newSquare = game.squareAt(newPos);
+		newSquare.addEntity(this);
+		if (m.getPushed() == true) boxSquare.pushContents(direction);
 	}
 	public String getName()
 	{
